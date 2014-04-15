aac0252793eb7017543dc19d185ed516edc700fd
Ge Hao <althoughghgh@gmail.com>
2014-04-15 20:54:10 +0800

    Add user microposts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,6 +11,8 @@ describe User do
 
   subject { @user }
 
+  it { should respond_to(:microposts) }
+  it { should respond_to(:feed) }
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
