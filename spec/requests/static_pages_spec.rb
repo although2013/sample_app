1f208348464e3b408c3a3ee97ccf0f678465967d
Ge Hao <althoughghgh@gmail.com>
2014-04-20 21:45:49 +0800

    before 11.2.4

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,6 +3,33 @@ require 'spec_helper'
 describe "StaticPages" do
 
 	describe "Home pages" do
+
+		describe "for signed-in users" do
+			let(:user) { FactoryGirl.create(:user) }
+			before do
+				FactoryGirl.create(:micropost, user: user, content: "Lorem")
+				FactoryGirl.create(:micropost, user: user, content: "Ipsum")
+				sign_in user
+				visit root_path
+			end
+			it "should render the user's feed" do
+				user.feed.each do |item|
+					expect(page).to have_selector("li##{item.id}", text: item.content)
+				end
+			end
+
+			describe "follower/following counts" do
+				let(:other_user) { FactoryGirl.create(:user) }
+				before do
+					other_user.follow!(user)
+					visit root_path
+				end
+				
+				it { should have_link("0 following", href: following_user_path(user)) }
+				it { should have_link("1 followers", href: followers_user_path(user)) }
+			end
+		end
+
 		it "should have the content 'Sample App'" do
 			visit root_path
 			expect(page).to have_content('Sample App')
